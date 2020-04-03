526be3ef3258ad51cfc1c90a12f38f943574109a
André Lashley <andre.lashley@gmail.com>
2020-04-03 16:36:17 -0700

    Check headines for null in Newsroom controller

--- Gov.News.WebApp/Controllers/Shared/NewsroomController.cs
+++ Gov.News.WebApp/Controllers/Shared/NewsroomController.cs
@@ -195,7 +195,7 @@ namespace Gov.News.Website.Controllers.Shared
                     model.Results.Add(new SearchViewModel.Result()
                     {
                         Title = System.Net.WebUtility.HtmlDecode(titles.FirstOrDefault().ToString()),
-                        Headline = System.Net.WebUtility.HtmlDecode(headlines.FirstOrDefault().ToString()),
+                        Headline = System.Net.WebUtility.HtmlDecode(headlines?.FirstOrDefault().ToString()),
                         Uri = NewsroomExtensions.GetPostUri(postKind.ToLower(), key),
                         Description = result["summary"],
                         HasMediaAssets = result["hasMediaAssets"],
