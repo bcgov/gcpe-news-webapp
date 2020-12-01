91897924bdf7c29b54ba28c1061f3b5d46e46201
André Lashley <andre.lashley@gmail.com>
2020-12-01 11:20:07 -0800

    Update date ranges for news archive links for new collection (#439)

--- Gov.News.WebApp/Models/SearchViewModel.cs
+++ Gov.News.WebApp/Models/SearchViewModel.cs
@@ -64,7 +64,8 @@ namespace Gov.News.Website.Models
         }
 
         public static Dictionary<string, string> DateCollections = new Dictionary<string, string> {
-                { "2017-2021", "July 18, 2017 to current date" },
+                { "2020-2024", "November 26, 2020 to current date" },
+                { "2017-2021", "July 18, 2017 to November 25, 2020" },
                 { "2017-2017", "June 12, 2017 to July 17, 2017" },
                 { "2013-2017", "June 10, 2013 to June 11, 2017" },
                 { "2009-2013", "March 12, 2011 to June 9, 2013" }
