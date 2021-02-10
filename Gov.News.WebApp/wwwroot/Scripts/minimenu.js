e422d8aa86db859753d7f878866ed896870a5574
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2021-02-10 14:14:31 -0800

    fix mobile button click (#467)
    * change mobile menu to bootstrap style
    
    * added boostrap
    
    * fixed the ministry dropdown
    
    * remove hover effect
    
    * fix the search in mobile view
    
    * fixed the mobile menu tag
    
    * fix the mobile buttons

--- Gov.News.WebApp/wwwroot/Scripts/minimenu.js
+++ Gov.News.WebApp/wwwroot/Scripts/minimenu.js
@@ -9,33 +9,7 @@
 
 $(function () {
 
-    $('#mini-menu-container').on('shown.bs.collapse', function () {
-        $('#hamburger-button').toggleClass('hidden');
-        $('#close-hamburger-button').toggleClass('hidden');
-    })
-    $('#mini-menu-container').on('hidden.bs.collapse', function () {
-        $('#hamburger-button').toggleClass('hidden');
-        $('#close-hamburger-button').toggleClass('hidden');
-    })
-    $('#mini-search-container').on('shown.bs.collapse', function () {
-        $('#search-button').toggleClass('hidden');
-        $('#close-search-button').toggleClass('hidden');
-        $('.mini-search-container-trigger').addClass('triggered');
-        $('.mini-menu-search').slideDown(200, function () { });
-
-    })
-    $('#mini-search-container').on('hidden.bs.collapse', function () {
-        $('#search-button').toggleClass('hidden');
-        $('#close-search-button').toggleClass('hidden');
-        $('.mini-search-container-trigger').removeClass('triggered');
-        $('.mini-menu-search').hide();
-    })
-
-    $("#ministry-dropdown").on('click', function () {
-
-        $('#ministry-dropdown-down').toggleClass('hidden');
-        $('#ministry-dropdown-up').toggleClass('hidden');
-    });
+   
     $(".level-trigger").on('click', function () {
         var childList = $(this).siblings().parent().find('ul:first');
         if (childList.length === 0)
